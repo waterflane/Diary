6f25bb4bf0624cb8d99332c25ec52b7e811b5198
Kirill <106469980+waterflane@users.noreply.github.com>
2024-12-27 20:26:04 +0300

    Hot-fix V 0.91
    Changes:
        - Fixing a bug where nothing was written to the file

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import datetime
 def write():
     print('Напишите небольшой текст на 1 предложение:')
     now_text = input()
-    f = open('diary/diary.txt', 'a', encoding='utf-8')
+    f = open('diary.txt', 'a', encoding='utf-8')
     f.write(f'{datetime.datetime.now()}--!--{now_text}\n')
     f.close()
 
 def read():
     try:
-        f = open('diary/diary.txt', 'r', encoding='utf-8')
+        f = open('diary.txt', 'r', encoding='utf-8')
     except FileNotFoundError:
-        f_helper = open('diary/diary.txt', 'a', encoding='utf-8')
+        f_helper = open('diary.txt', 'a', encoding='utf-8')
         print('Ничего оне найденно!')
         f_helper.close()
         print('Enter чтобы продолжить')
@@ -25,19 +25,19 @@ def read():
         print('Enter чтобы продолжить')
         now_text = input()
 
-def alarm_st():
-    print('Напишите напоминание')
-    now_text = input()
+# def alarm_st():
+#     print('Напишите напоминание')
+#     now_text = input()
 
-    print('Напишите дату, когда вам напомнить об этом в формате: Y, M, D')
-    now_text = input()
-    list_date = list(map(int, now_text.split(', ')))
-    date = datetime.date(list_date[0], list_date[1], list_date[2])
+#     print('Напишите дату, когда вам напомнить об этом в формате: Y, M, D')
+#     now_text = input()
+#     list_date = list(map(int, now_text.split(', ')))
+#     date = datetime.date(list_date[0], list_date[1], list_date[2])
 
-    f = open('diary/alarm_text.txt', 'a', encoding='utf-8')
-    f.write(f'{date}---!---{now_text} \n')
-    f.close()
-    text.ALARM_NEW = True
+#     f = open('diary/alarm_text.txt', 'a', encoding='utf-8')
+#     f.write(f'{date}---!---{now_text} \n')
+#     f.close()
+#     text.ALARM_NEW = True
 
 
 def main():
@@ -46,7 +46,7 @@ def main():
     match input():
         case 'w': write()
         case 'r': read()
-        case 'a': alarm_st()
+        # case 'a': alarm_st()
         case 'q': exit()
     main()
 
